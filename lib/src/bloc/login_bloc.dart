775b61ed25d9b23df3065c3aa4af63da3319eb49
Asalmerontkd <psrantonio@gmail.com>
2020-10-18 13:49:32 -0500

    Obtener el ultimo valor del stream

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -27,6 +27,10 @@ class LoginBloc with Validators {
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
 
+  // Obtener el ultimo valor del stream
+  String get email => _emailController.value;
+  String get password => _passwordController.value;
+
   dispose() {
     _emailController?.close();
     _passwordController?.close();
