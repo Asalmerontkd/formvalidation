4702ed7de6496ddd3c1d0d1560e9bc2f577d94f3
Asalmerontkd <psrantonio@gmail.com>
2020-10-20 13:13:05 -0500

    Nuevo formulario para alta de productos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -11,15 +11,18 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home Page')
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget> [
-          Text('Email: ${ bloc.email } '),
-          Divider(),
-          Text('Password: ${ bloc.password } '),
-        ],
+      body: Container(
+
       ),
+      floatingActionButton: _crearBoton(context),
+    );
+  }
+
+  Widget _crearBoton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () => Navigator.pushNamed(context, 'producto'),
+      backgroundColor: Colors.deepPurple,
     );
   }
 }
