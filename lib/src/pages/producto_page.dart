72ac117f4aa4091129f7f1fc38c239c94f7a3969
Asalmerontkd <psrantonio@gmail.com>
2020-10-22 00:21:52 -0500

    Validaciones de formulario

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/utils/utils.dart' as utils;
 
-class ProductoPage extends StatelessWidget {
+class ProductoPage extends StatefulWidget {
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +29,7 @@ class ProductoPage extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(15.0),
           child: Form(
+            key: formKey,
             child: Column(
               children: <Widget> [
                 _crearNombre(),
@@ -40,6 +49,13 @@ class ProductoPage extends StatelessWidget {
       decoration: InputDecoration(
         labelText: 'Producto'
       ),
+      validator: ( valor ){
+        if ( valor.length < 3 ){
+          return 'Ingrese el nombre del producto.';
+        }else{
+          return null;
+        }
+      },
     );
   }
 
@@ -49,6 +65,13 @@ class ProductoPage extends StatelessWidget {
       decoration: InputDecoration(
         labelText: 'Precio'
       ),
+      validator: ( valor ){
+        if ( utils.isNumeric(valor) ){
+          return null;
+        }else{
+          return 'Solo números';
+        }
+      },
     );
   }
 
@@ -61,7 +84,13 @@ class ProductoPage extends StatelessWidget {
       textColor: Colors.white,
       label: Text('Guardar'),
       icon: Icon( Icons.save ),
-      onPressed: () {}
+      onPressed: _submit
     );
+  }
+
+  void _submit(){
+    if ( !formKey.currentState.validate() ) return;
+
+    print('TODO OK');
   }
 }
